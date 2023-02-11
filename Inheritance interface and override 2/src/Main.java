24a7de50fa6c686c1335aee084fd81b38a4f7e90
Amalie <Amal956e@stud.kea.dk>
2023-02-11 23:39:37 +0100

    Ny rettelse

--- Inheritance interface and override 2/src/Main.java
+++ Inheritance interface and override 2/src/Main.java
@@ -6,15 +6,15 @@ import Override.Override1;
 public class Main {
     public static void main(String[] args) {
         //Inheritance 1 - In the main method create a Laptop and a SmartPhone
-        Inheritance1.inheritance1Exercise();
+        Inheritance1.inheritance1Exercise(); //Kalder laptop og smartphone objekt uden toString
 
         //Interface 1 - Add some different Fastfood objects to an array. Now iterate through that array and use some of the methods you have created above.
-        Interface1.InterfaceExercise_1();
+        Interface1.InterfaceExercise_1(); //Kalder sandwich, pizza og bruger med metoderne isFoodReady og foodIsExpired (om de er færdig og om de er udløbet)
 
         //Interface 2 - Create a class that implements the following interface. Now create two objects using the class created
-        Interface2.InterfaceExercise_2();
+        Interface2.InterfaceExercise_2(); //Kalder Tesla og Volvo med metoden changeGear, speedUp og applyBrakes
 
         //Override - In the Main class, create an instance of each class and call the play method on each
-        Override1.OverrideExercise();
+        Override1.OverrideExercise(); //Kalder RapSong, OldSchoolRap og NewSchoolRap med metoden "play();"
     }
 }
--- Inheritance interface and override 2/src/Override/Override1.java
+++ Inheritance interface and override 2/src/Override/Override1.java
@@ -2,13 +2,13 @@ package Override;
 
 public class Override1 {
     public static void OverrideExercise() {
-        RapSong newRapSong = new RapSong();
-        newRapSong.play(); //Playing a rap song
+        RapSong rapSong = new RapSong();
+        rapSong.play(); //"Playing a rap song"
 
-        OldSchoolRap newOldSchoolRap = new OldSchoolRap();
-        newOldSchoolRap.play(); //Playing old school rap
+        OldSchoolRap oldSchoolRap = new OldSchoolRap();
+        oldSchoolRap.play(); //"Playing old school rap"
 
-        NewSchoolRap newSchoolRap = new NewSchoolRap();
-        newSchoolRap.play();//Playing new school rap
+        NewSchoolRap schoolRap = new NewSchoolRap();
+        schoolRap.play();//"Playing new school rap"
     }
 }
